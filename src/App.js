ccfcd91c8588da04b2afa07cd6ca53bd7a34249d
zfl19 <19aleiya@gmail.com>
2024-05-26 22:38:54 +0800

    抽取组件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Hello from "./components/Hello";
+
 function App() {
   return (
       <div>
-        <h1>react-note</h1>
-        <span style={{fontSize:"20px"}}>基于React的记事待办app</span>
+        <Hello />
       </div>
   );
 }
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>react-note</h1>
+                <span style={{fontSize: "20px"}}>基于React的记事待办app</span>
+            </div>
+        )
+    }
+}
