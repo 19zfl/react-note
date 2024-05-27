1c71fcf6917c29b62525192157a9780e1c7303c0
zfl19 <19aleiya@gmail.com>
2024-05-27 20:10:20 +0800

    组件初始化

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import Hello from "./components/Hello";
-
 function App() {
   return (
       <div>
-        <Hello />
+
       </div>
   );
 }
---
+++ src/components/List/index.jsx
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
